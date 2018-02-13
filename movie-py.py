da81b876b95a2c47d179df4b255054e80cba1cba
Honza-m <jan.morawiec@outlook.com>
2018-02-13 17:21:16 +0000

    New info - metascore, content rating, release date.

--- movie-py.py
+++ movie-py.py
@@ -22,8 +22,13 @@ try:
     movietitle = titlenyear[0:len(titlenyear) - 8]
     movieyear = titlenyear[len(titlenyear) - 6:len(titlenyear) - 2]
     movierating = soup2.select(".ratingValue span")[0].text
+    metascore = soup2.select(".metacriticScore")
+    metascore = metascore[0].text.strip() if metascore else None
+    contentrating = soup2.find('meta',{'itemprop':'contentRating'})
+    contentrating = contentrating['content'].strip() if contentrating else None
     movielength = soup2.select(".subtext time")[0].text.strip()
     genresndate = [i.text for i in soup2.select(".subtext a")]
+    releasedate = genresndate[-1].strip()
     moviegenres = ""
     for x in range(len(genresndate) - 1):
         moviegenres = moviegenres + ',' + genresndate[x]
@@ -34,18 +39,15 @@ try:
     movieactors = moviecast[3] + ',' + moviecast[4] + ',' + moviecast[5];
 
     print(Fore.LIGHTRED_EX + "Title: " + Fore.LIGHTGREEN_EX + movietitle)
-    print(Fore.LIGHTRED_EX + "Rating: " + Fore.LIGHTYELLOW_EX + movierating + "/10")
+    print(Fore.LIGHTRED_EX + "IMDB Rating: " + Fore.LIGHTYELLOW_EX + movierating + "/10")
+    if metascore: print(Fore.LIGHTRED_EX + "Metascore: " + Fore.LIGHTYELLOW_EX + metascore + "/100")
     print(Fore.LIGHTRED_EX + "Length: " + Fore.LIGHTCYAN_EX + movielength)
     print(Fore.LIGHTRED_EX + "Year: " + Fore.LIGHTMAGENTA_EX + movieyear)
     print(Fore.LIGHTRED_EX + "Genre: " + Fore.LIGHTBLUE_EX + moviegenres)
     print(Fore.LIGHTRED_EX + "Description: " + Fore.LIGHTWHITE_EX + moviedesc)
+    print(Fore.LIGHTRED_EX + "Release date: " + Fore.LIGHTCYAN_EX + releasedate)
+    if contentrating: print(Fore.LIGHTRED_EX + "Rating: " + Fore.LIGHTCYAN_EX + contentrating)
     print(Fore.LIGHTRED_EX + "Director: " + Fore.LIGHTBLACK_EX + moviedirector)
     print(Fore.LIGHTRED_EX + "Lead Cast: " + Fore.LIGHTBLACK_EX + movieactors)
 except:
     print(Fore.LIGHTRED_EX+"Something's wrong,Try Again Later")
-
-
-
-
-
-
