af339f0aad444217d2356190ddfe2223443bc3a7
= <chevol@chevol.com>
2018-03-31 13:29:42 -0400

    Added Country,Also Known As,Budget,Opening Weekend USA,Gross USA,Cumulative Worldwide Gross,Ratio,and Taglines

--- movie-py.py
+++ movie-py.py
@@ -30,6 +30,16 @@ try:
     genresndate = [i.text for i in soup2.select(".subtext a")]
     releasedate = genresndate[-1].strip()
     moviegenres = ""
+    for i in soup2.find_all("div","txt-block"):
+        if i.h4:
+            if i.h4.text=="Budget:":moviebudget = i.h4.next_element.next_element.strip()
+            if i.h4.text=="Opening Weekend USA:":movieopening = i.h4.next_element.next_element.strip()[:-1]
+            if i.h4.text=="Gross USA:":movieusagross = i.h4.next_element.next_element.strip()[:-1]
+            if i.h4.text=="Cumulative Worldwide Gross:":movieworldgross = i.h4.next_element.next_element.strip()[:-1]
+            if i.h4.text=="Aspect Ratio:":movieratio = i.h4.next_element.next_element.strip()
+            if i.h4.text=="Taglines:":movietaglines = i.h4.next_element.next_element.strip()
+            if i.h4.text=="Also Known As:":moviealsoknown = i.h4.next_element.next_element.strip()
+            if i.h4.text=="Country:":moviecountry = i.h4.next_sibling.next_element.text.strip()
     for x in range(len(genresndate) - 1):
         moviegenres = moviegenres + ',' + genresndate[x]
     moviegenres = moviegenres[1:]
@@ -49,5 +59,13 @@ try:
     if contentrating: print(Fore.LIGHTRED_EX + "Rating: " + Fore.LIGHTCYAN_EX + contentrating)
     print(Fore.LIGHTRED_EX + "Director: " + Fore.LIGHTBLACK_EX + moviedirector)
     print(Fore.LIGHTRED_EX + "Lead Cast: " + Fore.LIGHTBLACK_EX + movieactors)
+    print(Fore.LIGHTRED_EX + "Country: " + Fore.LIGHTBLUE_EX + moviecountry)
+    print(Fore.LIGHTRED_EX + "Also Known As: " + Fore.LIGHTBLUE_EX + moviealsoknown)
+    print(Fore.LIGHTRED_EX + "Budget: " + Fore.LIGHTBLUE_EX + moviebudget)
+    print(Fore.LIGHTRED_EX + "Opening Weekend USA: " + Fore.LIGHTBLUE_EX + movieopening)
+    print(Fore.LIGHTRED_EX + "Gross USA: " + Fore.LIGHTBLUE_EX + movieusagross)
+    print(Fore.LIGHTRED_EX + "Cumulative Worldwide Gross: " + Fore.LIGHTBLUE_EX + movieworldgross)
+    print(Fore.LIGHTRED_EX + "Ratio: " + Fore.LIGHTBLUE_EX + movieratio)
+    print(Fore.LIGHTRED_EX + "Taglines: " + Fore.LIGHTBLUE_EX + movietaglines)
 except:
     print(Fore.LIGHTRED_EX+"Something's wrong,Try Again Later")
